e6d919274aebf3904ec07332b14e3d3143790a05
Trình Công Trình <congtrinh097@gmail.com>
2017-11-30 14:10:23 +0700

    Add Dashboard Not completed

---
+++ src/CTX.Web/ClientApp/components/Commons/ContentHeader.tsx
@@ -0,0 +1,19 @@
+﻿import * as React from 'react';
+
+export interface thisProps {
+    header: string,
+    description?: string
+}
+
+export class ContentHeader extends React.Component<thisProps, {}>{
+
+  render() {
+        return (
+            <div id="page-header" className="clearfix">
+                <div className="page-header">
+                    <h2>{this.props.header || ''}</h2>
+                    <span className="txt">{this.props.description || ''}</span>
+                </div>  
+            </div>);
+    }
+}
--- src/CTX.Web/ClientApp/components/Home/Index.tsx
+++ src/CTX.Web/ClientApp/components/Home/Index.tsx
@@ -1,34 +1,14 @@
 import * as React from 'react';
 import {RouteComponentProps} from 'react-router-dom';
+import {ContentHeader} from "../Commons/ContentHeader";
+
 
 export default class Home extends React.Component<RouteComponentProps<{}>, {}> {
   public render() {
     return (
       <div className="page-content-wrapper">
         <div className="page-content-inner">
-
-          <div id="page-header" className="clearfix">
-            <div className="page-header">
-              <h2>Bảng theo dõi</h2>
-              <span className="txt">Bảng theo dõi thông tin hệ thống</span>
-            </div>
-            <div className="header-stats">
-              <div className="spark clearfix">
-                <div className="spark-info"><span className="number">2345</span>Visitors</div>
-                <div id="spark-visitors" className="sparkline"></div>
-              </div>
-              <div className="spark clearfix">
-                <div className="spark-info"><span className="number">17345</span>Views</div>
-                <div id="spark-templateviews" className="sparkline"></div>
-              </div>
-              <div className="spark clearfix">
-                <div className="spark-info"><span className="number">3700$</span>Sales</div>
-                <div id="spark-sales" className="sparkline"></div>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-          </div>
+            <ContentHeader header='Trang chủ'  />
         </div>
       </div>);
   }
